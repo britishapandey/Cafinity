3cc24d8d45f5818711409d5f8f071fe17ff3cc1e
jaerlcruz <jaeraelcruz@gmail.com>
2024-12-16 15:42:33 -0800

    Added basic NavBar and Login UI

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,13 +1,19 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { Home, Star, User } from 'lucide-react';
 
 const Navbar = () => {
   return (
-    <div style={{ padding: "10px", backgroundColor: "#5B4A43" }}>
-        <h1 className="text-white">Navbar</h1>
-        <Link to="/" className="m-4">Home</Link>
-        <Link to="/login" className="m-4">Login</Link>
-        <Link to="/profile" className="m-4">Profile</Link>
+    <div className="bg-[#5B4A43] p-4">
+        <div className="flex justify-between items-center w-full px-8">
+            <h1 className="text-white text-2xl font-bold">Cafinity</h1>
+            <div className="flex">
+                <Link to="/" className="m-4"><Home color="#6490E1"/></Link>
+                <Link to="/profile" className="m-4"><Star color="#6490E1"/></Link>
+                <Link to="/login" className="m-4"><User color="#6490E1"/></Link>
+            </div>
+
+        </div>
     </div>
   );
 };
--- src/components/auth.jsx
+++ src/components/auth.jsx
@@ -1,8 +1,13 @@
 export const Auth = () => {
   return(
-    <div>
-      <input placeholder="Email..." />
-      <input placeholder="Password..." />
+    <div className="flex flex-col items-center justify-center h-screen">
+      <div>Welcome to</div>
+      <h1 className="text-3xl font-bold m-2">Cafinity!</h1>
+      <div className="flex flex-col m-2">
+        <input placeholder="Email..." />
+        <input placeholder="Password..." />
+      </div>
+      <button className="m-2 bg-[#B07242] text-[#E7E4E1] transition-colors hover:bg-[#6490E1]">Sign in</button>
     </div>
   )
 }

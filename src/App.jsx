ab99928c6069e34be987560b28c8d8515ad3179e
jaerlcruz <jaeraelcruz@gmail.com>
2024-12-16 19:31:09 -0800

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,3 @@
-import { useState, useEffect } from 'react';
-// import './App.css';
-import './index.css';
-import { Auth } from './components/auth';
-import { db } from './config/firebase';
-import { addDoc, collection, getDocs } from 'firebase/firestore';
-import { useRoutes } from 'react-router-dom';
-import Navbar from './components/NavBar';
-import Home from './components/Home';
-
-function App() {
-  let element = useRoutes([
-    { path: '*', element: <Home /> },
-    { path: '/login', element: <Auth /> },
-    // { path: '/profile', element: <Profile /> },
-  ])
-  return (
-    <>
-      {element}
-    </>
 import { db, auth } from './config/firebase'; // Firebase config
 import { addDoc, collection, getDocs } from 'firebase/firestore';
 import { BrowserRouter as Router, Routes, Route, Link, Navigate } from 'react-router-dom';
